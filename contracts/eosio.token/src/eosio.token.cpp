9ca278d80d0273e2537f56b9c8a442d21d63eb29
sirius4galaxy <uniswap_v2@163.com>
2024-05-07 15:00:53 +0800

    add xshard transfer feature for token contract

--- contracts/eosio.token/src/eosio.token.cpp
+++ contracts/eosio.token/src/eosio.token.cpp
@@ -1,4 +1,6 @@
 #include <eosio.token/eosio.token.hpp>
+#include <eosio/transaction.hpp>
+
 
 namespace eosio {
 
@@ -153,6 +155,48 @@ void token::close( const name& owner, const symbol& symbol )
    check( it != acnts.end(), "Balance row already deleted or never existed. Action won't have any effect." );
    check( it->balance.amount == 0, "Cannot close because the balance is not zero." );
    acnts.erase( it );
+}
+
+void token::xshout( const name& owner, const name& to_shard, const asset& quantity, const string& memo ) {
+
+    require_auth( owner );
+    require_auth( system_account );
+    auto sym = quantity.symbol.code();
+    stats statstable( get_self(), sym.raw() );
+    // TODO: issuer must init the currency in to shard before xshout
+    const auto& st = statstable.get( sym.raw() );
+
+   //  require_recipient( owner );
+
+    check( quantity.is_valid(), "invalid quantity" );
+    check( quantity.amount > 0, "must transfer positive quantity" );
+    check( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
+    check( memo.size() <= 256, "memo has more than 256 bytes" );
+
+    sub_balance( owner, quantity );
+
+   // TODO:  st.shard_balance -= quantity;
+}
+
+void token::xshin( const name& owner, const name& from_shard, const asset& quantity, const string& memo ) {
+
+    require_auth( owner );
+    require_auth( system_account );
+   //  check(get_first_receiver() == system_account, "this action can only be called by system_account");
+   //  auto sym = quantity.symbol.code();
+   //  stats statstable( get_self(), sym.raw() );
+    // TODO: issuer must init the currency in to shard before xshout
+   //  const auto& st = statstable.get( sym.raw() );
+
+   //  require_recipient( owner );
+
+   // check( quantity.is_valid(), "invalid quantity" );
+   check( quantity.amount > 0, "must transfer positive quantity" );
+   // check( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
+   check( memo.size() <= 256, "memo has more than 256 bytes" );
+
+   add_balance( owner, quantity, owner ); // TODO: payer??
+   // TODO: st.shard_balance += quantity;
 }
 
 } /// namespace eosio
